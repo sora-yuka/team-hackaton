8147ad67938c6017d76f0cc001929bc19fdfb62c
sora-yuka <feelingjeez@gmail.com>
2022-12-27 19:07:32 +0600

    custom viewsets for order, product, favorite - 1.0, core with custom mixins and viewsets

--- applications/account/views.py
+++ applications/account/views.py
@@ -27,20 +27,6 @@ class RegisterAPIView(APIView):
                         )
         
 
-
-class ActivationApiView(APIView):
-    def get(self, request, activation_code):
-        try:
-            user = User.objects.get(activation_code=activation_code)
-            user.is_active = True
-            user.activation_code = ''
-            user.save()
-            return Response({'message': 'successfully'}, status=status.HTTP_200_OK)
-        except User.DoesNotExist:
-            return Response({'message': 'Wrong email!'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-
 class ActivationApiView(APIView):
     def get(self, request, activation_code):
         try:
