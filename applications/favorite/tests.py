8e72b3b57460cb213496660b565d98543868f88c
sora-yuka <feelingjeez@gmail.com>
2023-01-07 00:44:11 +0600

    test for favorite 1.6

--- applications/favorite/tests.py
+++ applications/favorite/tests.py
@@ -7,8 +7,6 @@ from django.urls import reverse
 
 
 class FavoriteTest(TestCase):
-    url = "http://127.0.0.1:8000/api/v1/favorites/"
-    
     def setUp(self):
         CustomUser.objects.create(email="example@mail.com", password="qwerty", is_active=True)
         self.product = Product.objects.create(name="Test", price=1000, category="Education", amount=2, owner=CustomUser.objects.get(email="example@mail.com"))
@@ -29,15 +27,17 @@ class FavoriteTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         
     def test_favorite_retrieve(self):
-        response = self.client.post(self.url, data={"product": 1}, **self.bearer_token)
+        url = "http://127.0.0.1:8000/api/v1/favorites/"
+        response = self.client.post(url, data={"product": "1"}, **self.bearer_token)
         
-        url = "http://127.0.0.1:8000/api/v1/favorites/1/"
-        response = self.client.get(url, ** self.bearer_token)
+        url = f"http://127.0.0.1:8000/api/v1/favorites/{response.data['id']}/"
+        response = self.client.get(url, **self.bearer_token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-    
-    def test_favorite_destroy(self):
-        response = self.client.post(self.url, data={"product": 1}, **self.bearer_token)
         
-        url = "http://127.0.0.1:8000/api/v1/favorites/1/"
-        response = self.client.delete(url, ** self.bearer_token)
+    def test_favorite_destroy(self):
+        url = "http://127.0.0.1:8000/api/v1/favorites/"
+        response = self.client.post(url, data={"product": 1}, **self.bearer_token)
+        
+        url = f"http://127.0.0.1:8000/api/v1/favorites/{response.data['id']}/"
+        response = self.client.delete(url, **self.bearer_token)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
